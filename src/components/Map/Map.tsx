c39ec744f7413fca88369f6d8caeb4bb864dca2b
Kuba Kwiatkowski <jakub.kwiatkowski1981@gmail.com>
2022-05-17 12:45:35 +0200

    added Leaflet Maps to my App

--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import './Map.css';
+import {MapContainer, TileLayer} from "react-leaflet";
+import 'leaflet/dist/leaflet.css';
+
 
 export const Map = () => {
 
     return (
         <>
             <div className="map">
-                <h1>Mapa...</h1>
+                <MapContainer center={[47.3729666,8.5058228]} zoom={13}>
+                    <TileLayer
+                        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> & contributors'
+                        url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'/>
+                </MapContainer>
 
             </div>
         </>
